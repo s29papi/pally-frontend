08f84efe7781f0282cf3b919c52141e630246857
S29 <nots29vv@gmail.com>
2024-07-02 15:23:07 +0100

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-
+"use client"
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 900);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
     <main className="flex w-full h-full">
-         Hello, Pally 
+         {
+          loading
+            ?
+            <div className='flex'>Be r</div>
+            :
+            <div className='flex'>Jai</div>
+         }
     </main>
   );
 }
