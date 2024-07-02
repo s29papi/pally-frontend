b041a1f5f56d8b85715ff4077a0f5ae5796733c5
S29 <nots29vv@gmail.com>
2024-07-02 17:18:40 +0100

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 "use client"
 import { useState, useEffect } from 'react';
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -12,15 +19,30 @@ export default function Home() {
     return () => clearTimeout(timer); 
   }, []);
 
+  if (!process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID) {
+    return 
+  }
+  
   return (
+    <DynamicContextProvider
+    settings={{
+      // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+      environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID,
+      walletConnectors: [EthereumWalletConnectors],
+    }}
+  >
     <main className="flex w-full h-full">
-         {
-          loading
-            ?
-            <div className='flex'>Be r</div>
-            :
-            <div className='flex'>Jai</div>
-         }
-    </main>
+{
+ loading
+   ?
+   <div className='flex'>Be r</div>
+   :
+   <div className='flex'>Jai</div>
+}
+</main>
+    <DynamicWidget  />
+  </DynamicContextProvider>
   );
 }
+
+
